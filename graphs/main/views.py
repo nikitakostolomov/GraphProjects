7bc6a8bbd73ea13716c31a623f5684f79dcc5854
dasha <yermolovadarya@gmail.com>
2021-05-26 23:27:39 +0300

    add css

--- graphs/main/views.py
+++ graphs/main/views.py
@@ -4,6 +4,18 @@ import ctypes, pathlib, json, os.path
 from ctypes import cdll
 from PIL import Image
 from numpy import asarray
+import itertools
+
+def serialize(obj):
+    return (json.dumps(str(obj)).replace('"', ''))
+
+def convert_str_to_list(str):
+    list_of_pairs=[]
+    list_of_coords=list(str.split(","))
+    for i in range(0,len(list_of_coords)-1,2):
+        list_of_pairs.append((int(list_of_coords[i]),int(list_of_coords[i+1])))
+    return list_of_pairs
+
 
 def mainpage(request):
     if request.method == 'POST':
@@ -14,37 +26,37 @@ def mainpage(request):
     else:
         form = ImageForm()
 
-    img = request.session['image']
+    img_session = request.session['image']
     data = {
-        'form': form,
-        'img': img,
-        'is_download_img': os.path.exists('media/'+img)
+        'img': img_session,
+        'is_download_img': os.path.exists('media/'+img_session)
         
     }
     return render(request, 'main/mainpage.html', context = data)
 
-def serialize(obj):
-    return (json.dumps(str(obj)).replace('"', ''))
-
 def segmentation(request):
-    img=request.session['image']
-    # # Преобразование изображения в массив
-    # img = Image.open('media/'+img)
-    # data = asarray(img)
-    # # Обратно
     # img_out = Image.fromarray(data)
 
     if request.method == 'POST':
-        object_pixels = request.POST.get('object_pixels', False)
-        background_pixels = request.POST.get('background_pixels', False)
-        print(type(object_pixels))
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            request.session['image']= serialize(form.instance.image)
+        else:
+            form = ImageForm()
+        # object_pixels = convert_str_to_list(request.POST.get('object_pixels', ''))
+        # background_pixels = convert_str_to_list(request.POST.get('background_pixels', ''))
+        # img = Image.open('media/'+img_session)
+        # data = asarray(img)
+
+    img_session=request.session['image']
     data = {
-        'img': img,
-        'typeofpage' : 'segmenation',
-        'is_download_img': os.path.exists('media/'+img)
+        'img': img_session,
+        'is_download_img': os.path.exists('media/'+img_session)
     }
-        
-        
+          
     return render(request, 'main/mainpage.html',context = data)
+
+
 
 
